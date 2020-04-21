fba5114fda14323bd49a98a2515919ca98527c34
Nikos Kontakis <wirednkod@gmail.com>
2020-04-21 09:11:42 +0300

    various smaller or bigger changes (antd, logger)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Button, Radio } from 'antd'
+import { DownloadOutlined } from '@ant-design/icons'
+import { SizeType } from 'antd/lib/config-provider/SizeContext'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+type SizeState = {
+  size: String
+}
+
+class App extends PureComponent<{}, SizeState> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      size: 'large'
+    }
+  }
+
+  componentDidMount () {
+    this.setState({ size: 'large' })
+  }
+
+  handleSizeChange = (e:any) => {
+    this.setState({ size: e.target.value });
+  }
+  render() {
+    let size = this.state.size
+    return (
+      <div className="App">
+        <header className="App-header">
+        <div>
+          <Radio.Group value={size} onChange={this.handleSizeChange}>
+            <Radio.Button value="large">Large</Radio.Button>
+            <Radio.Button value="default">Default</Radio.Button>
+            <Radio.Button value="small">Small</Radio.Button>
+          </Radio.Group>
+          <br />
+          <br />
+          <Button type="primary" size={size}>
+            Primary
+          </Button>
+          <Button size={size}>Default</Button>
+          <Button type="dashed" size={size}>
+            Dashed
+          </Button>
+          <br />
+          <Button type="link" size={size}>
+            Link
+          </Button>
+          <br />
+          <Button type="primary" icon={<DownloadOutlined />} size={size} />
+          <Button type="primary" shape="circle" icon={<DownloadOutlined />} size={size} />
+          <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size} />
+          <Button type="primary" shape="round" icon={<DownloadOutlined />} size={size}>
+            Download
+          </Button>
+          <Button type="primary" icon={<DownloadOutlined />} size={size}>
+            Download
+          </Button>
+        </div>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 const loggerMiddleware = (req: Request, resp: Response, next: NextFunction) => {
-    console.log('Request logged:', req.method, req.path)
+    console.log('Request:', req.method, req.path)
     next()
 }
 
