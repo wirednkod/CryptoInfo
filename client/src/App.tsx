77e665dad982e3262fb0f19b3e9999eafa3c4e71
Nikos Kontakis <wirednkod@gmail.com>
2020-07-08 23:14:15 +0300

    Fix various client package.json thingies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ class App extends PureComponent {
     this.setState({ size: e.target.value });
   }
   render() {
-    let size = this.state.size
+    let { size } = this.state
     return (
       <div className="App">
         <header className="App-header">
