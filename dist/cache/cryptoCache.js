55b7c7b79bc09c28d453475d1f6dfbb79ed3b07d
Nikos Kontakis <wirednkod@gmail.com>
2020-07-27 16:37:00 +0200

    merge

--- dist/cache/cryptoCache.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const NodeCache = require('node-cache');
-const cryptoCache = new NodeCache();
-exports.default = cryptoCache;
-//# sourceMappingURL=cryptoCache.js.map
--- dist/helpers/dataStructre.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const structureGlobal = (req) => {
-    console.log(req);
-    return '123';
-};
-exports.default = structureGlobal;
-//# sourceMappingURL=dataStructre.js.map
